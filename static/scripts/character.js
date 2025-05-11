0418e1be605b8faf9d8e00f5e43a5a5309c64bf0
null <null@null>
2025-05-11 07:31:56 +0000

    new characters, thanks gyudon!

--- static/scripts/character.js
+++ static/scripts/character.js
@@ -146,6 +146,8 @@ export const characters = {
     foe: new Character("foe", "svg", false),
     kimono_giko: new Character("kimono_giko", "svg", false),
     kimono_shii: new Character("kimono_shii", "svg", false),
+    okabe_giko: new Character("okabe_giko", "svg", false),
+    kurisu_shii: new Character("kurisu_shii", "svg", false),
     negativ: new Character("negativ", "png", false),    
     sonichu: new Character("sonichu", "png", false),
     yume: new Character("yume", "png", false),
